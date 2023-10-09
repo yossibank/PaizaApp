d0b7f8f573fc0e33a41f126983ac224e5931b89f
yossibank <yossibank@yahoo.co.jp>
2023-10-09 21:16:39 +0900

    create project

---
+++ PaizaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaizaApp
+//
+//  Created by Kamiyama Yoshihito on 2023/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaizaApp/PaizaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaizaAppApp.swift
+//  PaizaApp
+//
+//  Created by Kamiyama Yoshihito on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct PaizaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

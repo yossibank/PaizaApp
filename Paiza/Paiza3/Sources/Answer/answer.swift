fe872b968b391df1dc05120d98f7c19343b41ab5
yossibank <yossibank@yahoo.co.jp>
2023-10-11 21:39:40 +0900

    create paiza3

---
+++ Paiza/Paiza3/Sources/Answer/answer.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+func answer(names: [String]) {
+    print("Hello \(names.joined(separator: ",")).")
+}
--- Paiza/Paiza3/Sources/main.swift
+++ Paiza/Paiza3/Sources/main.swift
@@ -1,4 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// 【問題】
+// 名前が複数格納できる配列namesがあります
+// namesの配列の文字列を「,」で結合し、先頭に「Hello」、末尾に「.」を結合した文字列を出力してください
 
-print("Hello, world!")
+func test(names: [String]) {
+    print(names)
+}
+
+// 入力例1: 「names: ["yossibank"]」 出力例1: 「Hello yossibank.」
+// 入力例2: 「names: ["Bob", "Tom", "Alice"]」 出力例2: 「Hello Bob,Tom,Alice.」
+
+test(names: ["yossibank"])

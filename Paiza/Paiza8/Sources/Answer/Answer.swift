aa6f9966525cba7f4af74d1c97aefdde37894275
yossibank <yossibank@yahoo.co.jp>
2023-10-21 13:41:42 +0900

    create paiza8

---
+++ Paiza/Paiza8/Sources/Answer/Answer.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+func answer(s: String, t: String) {
+    print("\(s)@\(t)")
+}
---
+++ Paiza/Paiza8/Sources/main.swift
@@ -0,0 +1,12 @@
+// 【問題: メールアドレスの作成】
+// メールアドレスのローカル部をs、ドメインをtとして文字列が入力されます
+// ローカル部とドメインを＠でつなげてメールアドレスを表示するように出力してください
+
+func test(s: String, t: String) {
+    print(s, t)
+}
+
+// 入力例1: 「s: info, t: paiza.jp]」 出力例1: 「info＠paiza.jp」
+// 入力例2: 「s: paiza, t: example.com]」 出力例2: 「paiza＠example.com」
+
+test(s: "s", t: "t")

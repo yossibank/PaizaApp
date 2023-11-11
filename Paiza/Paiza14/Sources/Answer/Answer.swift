3f741e59b9a82b93f1631c8d5f5bfd568a9e35dc
yossibank <yossibank@yahoo.co.jp>
2023-11-11 11:19:27 +0900

    create paiza14

---
+++ Paiza/Paiza14/Sources/Answer/Answer.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+func answer(s: String, n: Int) {
+    print(s.prefix(n))
+}
---
+++ Paiza/Paiza14/Sources/main.swift
@@ -0,0 +1,12 @@
+// 【問題: N文字目まで出力】
+// 半角アルファベットの小文字で構成された文字列sと正の整数nが入力されます
+// 1文字目からn文字目までを出力してください
+
+func test(s: String, n: Int) {
+    print(s, n)
+}
+
+// 入力例1: 「s: aabbccdd, n: 5」 出力例1: 「aabbc」
+// 入力例2: 「s: oiuytrer, n: 8」 出力例1: 「oiuytrer」
+
+test(s: "s", n: 1)

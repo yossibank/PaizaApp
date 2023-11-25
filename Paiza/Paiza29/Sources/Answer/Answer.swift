9d3ca3baf68ca3434d530e2141a37dff0cfa4103
yossibank <yossibank@yahoo.co.jp>
2023-11-25 15:42:49 +0900

    create paiza29

---
+++ Paiza/Paiza29/Sources/Answer/Answer.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func answer(n: Int) {
+    switch n {
+    case 1: print("January")
+    case 2: print("Feburary")
+    case 3: print("March")
+    case 4: print("April")
+    case 5: print("May")
+    case 6: print("June")
+    case 7: print("July")
+    case 8: print("August")
+    case 9: print("September")
+    case 10: print("October")
+    case 11: print("November")
+    case 12: print("December")
+    default: break
+    }
+}
